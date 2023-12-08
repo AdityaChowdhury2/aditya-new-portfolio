3350d978e8b52ee29ca4e18c6153e442815e2c28
AdityaChowdhury2 <adityac5102@gmail.com>
2023-12-08 17:38:20 +0600

    header added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,47 @@
 import { Link } from 'react-scroll';
 // import { RiMenuFill } from 'react-icons/ri';
 // import { useState } from 'react';
+import { Navbar } from 'flowbite-react';
 
 const Header = () => {
 	const navItems = ['projects', 'about-me', 'contacts'];
 
 	return (
-		<Navbar fluid rounded>
-			<Navbar.Brand as={Link} href="https://flowbite-react.com">
-				<span className="self-center whitespace-nowrap text-xl font-semibold">
-					~Aditya
-				</span>
-			</Navbar.Brand>
-			<Navbar.Toggle />
-			<Navbar.Collapse>
-				{navItems.map((navItem, idx) => (
-					<Link key={idx} href={navItem}>
-						{navItem}
-					</Link>
-				))}
-			</Navbar.Collapse>
-		</Navbar>
+		<div className="container">
+			<Navbar
+				fluid
+				rounded
+				className="bg-transparent text-white pt-8"
+				theme={{
+					collapse: {
+						list: 'mt-4 flex flex-col gap-y-8 md:mt-0 md:flex-row md:space-x-12 md:text-sm md:font-medium',
+						hidden: {
+							on: 'hidden',
+							off: '',
+						},
+					},
+				}}
+			>
+				<Navbar.Brand>
+					<span className="self-center whitespace-nowrap text-4xl font-semibold">
+						~Aditya
+					</span>
+				</Navbar.Brand>
+				<Navbar.Toggle />
+				<Navbar.Collapse>
+					{navItems.map((navItem, idx) => (
+						<Navbar.Link key={idx}>
+							<Link to={navItem}>
+								<p className="text-gray">
+									<span className="text-primary">#</span>
+									{navItem}
+								</p>
+							</Link>
+						</Navbar.Link>
+					))}
+				</Navbar.Collapse>
+			</Navbar>
+		</div>
 	);
 };
 
